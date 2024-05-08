4c9ca834b63cb4740a8569bb56af6dc13486b3ea
RuslanGon <ruslan441006@gmail.com>
2024-05-08 16:37:43 +0300

    wer

--- src/photos-api.js
+++ src/photos-api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
-import { PhotoData } from './types';
+import { PhotoData } from "./types";
 
 
 
 
 export async function fetchPhotosByInput (inputSearch: string, page: number):Promise<PhotoData>{
-   const npm install --save-dev typescript @types/react @types/react-dom
-ACCESS_KEY = 'Lo9uorFXpgT_Y7XkOmyvCmdEbDFs7mI4M-i24Hz4fks';
+   const ACCESS_KEY = 'Lo9uorFXpgT_Y7XkOmyvCmdEbDFs7mI4M-i24Hz4fks';
   const url:string = `https://api.unsplash.com/search/photos?query=${inputSearch}&client_id=${ACCESS_KEY}`;
   const response = await axios.get<PhotoData>(url, {
     params: {
@@ -14,6 +13,6 @@ ACCESS_KEY = 'Lo9uorFXpgT_Y7XkOmyvCmdEbDFs7mI4M-i24Hz4fks';
       page: page
     },
   });
-  const photos: PhotoData = response.data;
+  const photos:PhotoData = response.data;
   return photos;
 }
